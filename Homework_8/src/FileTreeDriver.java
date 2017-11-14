7b1a0b86c208b6ef63e3511afd7ab1dcaf2476c9
RZiserman <rziserman@gmail.com>
2017-11-14 03:01:08 +0000

    Remove tab delimiter

--- Homework_8/src/FileTreeDriver.java
+++ Homework_8/src/FileTreeDriver.java
@@ -2,6 +2,7 @@ import java.io.File;
 
 public class FileTreeDriver {
     public static void main(String[] args) {
-        FileTree.printTree(File.listRoots(), 0);
+       File[] fileArray = new File[]{new File("/home/rziserman/CISC3150_HW")};
+			 FileTree.printTree(fileArray, 0);
     }
 }
