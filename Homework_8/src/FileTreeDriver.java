7a3a49287dbf4c1c98f704b8355a34d3d4c84bef
RZiserman <rziserman@gmail.com>
2017-11-14 21:13:33 -0500

    add comments, fix format

--- Homework_8/src/FileTreeDriver.java
+++ Homework_8/src/FileTreeDriver.java
@@ -1,10 +1,14 @@
+//Author : Roman Ziserman
+/*
+ Create a file dir_tree.txt. Put in directory or directories of choice into the the printTree function.
+*/
+
 import java.io.*;
 
 public class FileTreeDriver {
     public static void main(String[] args) {
        try{
-       		BufferedWriter dir_tree = 
-							new BufferedWriter(new FileWriter("dir_tree.txt"));
+       		BufferedWriter dir_tree = new BufferedWriter(new FileWriter("dir_tree.txt"));
 	        File[] fileArray = new File[]{new File("/home/rziserman/CISC3150_HW")};
        		FileTree.printTree(fileArray, 0, dir_tree);        
        } catch (IOException e){
